f0cbf7fd70276287a044174c6497953fdcb894a9
Marko Mitrović <53355776+marko-mitrovic@users.noreply.github.com>
2020-02-24 13:02:09 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,115 @@
+/* First change */
+
+function colorChangePinkFirst() {
+    document.getElementById("titlePinkFirst").style.color = "#ff0072";
+    document.getElementById("linePinkFirst").style.borderColor = "#ff0072";
+    document.getElementById("titlePurpleFirst").style.color = "#fff";
+    document.getElementById("linePurpleFirst").style.borderColor = "#cccaca";
+    document.getElementById("titleBlueFirst").style.color = "#fff";
+    document.getElementById("lineBlueFirst").style.borderColor = "#cccaca";
+    document.getElementById("textChangeFirst").innerHTML = 'Multifunkcijski panel sa držačima za grafičke kartice mudro skriva kablove, pruža čvrst oslonac grafičkim karticama i omogućava montažu SSD uređaja ili <a href="https://www.asus.com/Motherboard-Accessories/ROG-Aura-Terminal/" target="_blank"> <span class="href-one"> ROG Aura Terminala</span></a> ukoliko želite da istaknete neki od ovih uređaja. Pozicija panela se može podešavati kako biste ga prilagodili ATX ili EATX matičnim pločama.';
+    document.getElementById('imageChangeFirst').src="./images/pic_Multi_Function_Cover.jpg";
+}
+
+function colorChangePurpleFirst() {
+    document.getElementById("titlePinkFirst").style.color = "#fff";
+    document.getElementById("linePinkFirst").style.borderColor = "#cccaca";
+    document.getElementById("titlePurpleFirst").style.color = "#8c64eb";
+    document.getElementById("linePurpleFirst").style.borderColor = "#8c64eb";
+    document.getElementById("titleBlueFirst").style.color = "#fff";
+    document.getElementById("lineBlueFirst").style.borderColor = "#cccaca";
+    document.getElementById("textChangeFirst").innerHTML = 'Fleksibilni dvodelni poklopac napajanja ima izrez na prednjoj strani, kako bi bio maksimalno kompatibilan sa napajanjima iz ROG Thor serije ili drugim napajanjima koja integrisan OLED ekran.';
+    document.getElementById('imageChangeFirst').src="./images/pic_PSU_Shroud.jpg";
+}
+
+function colorChangeBlueFirst() {
+    document.getElementById("titlePinkFirst").style.color = "#fff";
+    document.getElementById("linePinkFirst").style.borderColor = "#cccaca";
+    document.getElementById("titlePurpleFirst").style.color = "#fff";
+    document.getElementById("linePurpleFirst").style.borderColor = "#cccaca";
+    document.getElementById("titleBlueFirst").style.color = "#2049ff";
+    document.getElementById("lineBlueFirst").style.borderColor = "#2049ff";
+    document.getElementById("textChangeFirst").innerHTML = 'Prozračan poleđinski panel skriva gomilu kablova, dok providan materijal od kojeg je izrađen omogućava da RGB osvetljenje sa <a href="https://www.asus.com/Motherboard-Accessories/ROG-Aura-Terminal/" target="_blank"> <span class="href-three">ROG Aura Terminal</span></a>, SSD-a ili drugih uređaja bude vidljivo čak i kroz panel.';
+    document.getElementById('imageChangeFirst').src="./images/pic_Rear_Cable_Cover.jpg";
+}
+
+/* Second change */
+
+function colorChangePinkSecond() {
+    document.getElementById("titlePinkSecond").style.color = "#ff0072";
+    document.getElementById("linePinkSecond").style.borderColor = "#ff0072";
+    document.getElementById("titlePurpleSecond").style.color = "#fff";
+    document.getElementById("linePurpleSecond").style.borderColor = "#cccaca";
+    document.getElementById("titleBlueSecond").style.color = "#fff";
+    document.getElementById("lineBlueSecond").style.borderColor = "#cccaca";
+    document.getElementById("textChangeSecond").innerHTML = 'Samo jedan pritisak na dugmad za oslobađanje bočnih panela, koja su smeštena na zadnjoj strani kućišta, vam omogućava da lako skinete bočnu stranicu i priđete unutrašnjosti vašeg računara, bez korišćenja bilo kakvog alata. Paneli od kaljenog stakla se svojom donjom ivicom drže za kućište, što znači da neće spasti, odnosno da će biti otvoreni pod određenim uglom, sve dok ih u potpunosti ne odvojite od kućišta.';
+    document.getElementById('imageChangeSecond').src="./images/pic_Panel_Release_Buttons.png";
+}
+
+function colorChangePurpleSecond() {
+    document.getElementById("titlePinkSecond").style.color = "#fff";
+    document.getElementById("linePinkSecond").style.borderColor = "#cccaca";
+    document.getElementById("titlePurpleSecond").style.color = "#8c64eb";
+    document.getElementById("linePurpleSecond").style.borderColor = "#8c64eb";
+    document.getElementById("titleBlueSecond").style.color = "#fff";
+    document.getElementById("lineBlueSecond").style.borderColor = "#cccaca";
+    document.getElementById("textChangeSecond").innerHTML = 'Dva odvojiva nosača vam omogućavaju jednostavnu montažu ventilatora i radijatora, izvan računara, tj. pre nego što kompletnu jedinicu smestite u kućište, čime se pojednostavljuje sklapanje računara.';
+    document.getElementById('imageChangeSecond').src="./images/pic_Fan_Radiator_Mount.png";
+}
+
+function colorChangeBlueSecond() {
+    document.getElementById("titlePinkSecond").style.color = "#fff";
+    document.getElementById("linePinkSecond").style.borderColor = "#cccaca";
+    document.getElementById("titlePurpleSecond").style.color = "#fff";
+    document.getElementById("linePurpleSecond").style.borderColor = "#cccaca";
+    document.getElementById("titleBlueSecond").style.color = "#2049ff";
+    document.getElementById("lineBlueSecond").style.borderColor = "#2049ff";
+    document.getElementById("textChangeSecond").innerHTML = 'Filteri koji se nalaze na gornjoj, prednjoj i donjoj stranici kućišta predstavljaju sveobuhvatnu zaštitu računara od prašine. Jednostavno se uklanjaju sa računara kada dođe vreme za brisanje i čišćenje.';
+    document.getElementById('imageChangeSecond').src="./images/pic_Dust_Filters.png";
+}
+
+/* Third change */
+
+function colorChangePinkThird() {
+    document.getElementById("textColorPink").style.color = "#ff0072";
+    document.getElementById("textColorBlue").style.color = "#fff";
+    document.getElementById("versatileGraphic").src="./images/pic_FLEXIBLE_GRAPHICS_a.png";
+}
+
+function colorChangeBlueThird() {
+    document.getElementById("textColorBlue").style.color = "#2049ff";
+    document.getElementById("textColorPink").style.color = "#fff";
+    document.getElementById("versatileGraphic").src="./images/pic_FLEXIBLE_GRAPHICS_b.png";
+}
+
+/* Fourth change */
+
+function colorChangePinkFourth() {
+    document.getElementById("titlePinkThird").style.color = "#ff0072";
+    document.getElementById("linePinkThird").style.borderColor = "#ff0072";
+    document.getElementById("titlePurpleThird").style.color = "#fff";
+    document.getElementById("linePurpleThird").style.borderColor = "#cccaca";
+    document.getElementById("titleBlueThird").style.color = "#fff";
+    document.getElementById("lineBlueThird").style.borderColor = "#cccaca";
+    document.getElementById("imageChangeFourth").src="./images/lastone.jpg";
+}
+
+function colorChangePurpleFourth() {
+    document.getElementById("titlePinkThird").style.color = "#fff";
+    document.getElementById("linePinkThird").style.borderColor = "#cccaca";
+    document.getElementById("titlePurpleThird").style.color = "#8c64eb";
+    document.getElementById("linePurpleThird").style.borderColor = "#8c64eb";
+    document.getElementById("titleBlueThird").style.color = "#fff";
+
+    document.getElementById("imageChangeFourth").src="./images/lasttwo.jpg";
+}
+
+function colorChangeBlueFourth() {
+    document.getElementById("titlePinkThird").style.color = "#fff";
+    document.getElementById("linePinkThird").style.borderColor = "#cccaca";
+    document.getElementById("titlePurpleThird").style.color = "#fff";
+    document.getElementById("linePurpleThird").style.borderColor = "#cccaca";
+    document.getElementById("titleBlueThird").style.color = "#2049ff";
+    document.getElementById("lineBlueThird").style.borderColor = "#2049ff";
+    document.getElementById("imageChangeFourth").src="./images/lastthree.jpg";
+}
